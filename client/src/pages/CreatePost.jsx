8b611ab6a0f3cd006000e0d447da7795ee18620b
Siddharth Chaturvedi <sidd84755@gmail.com>
2023-03-23 21:24:28 +0530

    worked on posts, formfields

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -5,10 +5,57 @@ import { getRandomPrompt } from '../utils'
 import { FormField, Loader } from '../components'
 
 const CreatePost = () => {
+  const navigate = useNavigate();
+  const [form,setForm] = useState({
+    name:'',
+    prompt:'',
+    photo:'',
+  });
+  const [generatingImg, setGeneratingImg] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const handleSubmit = () => {
+    
+  }
+  const handleChange = (e) => {
+
+  }
+  const handleSurpriseMe = () => {
+
+  }
+
   return (
-    <div>
-      hi
-    </div>
+    <section className='max-w-7xl mx-auto'>
+      <div>
+        <h1 className='font-extrabold text-[#222328] text-[32px]'>
+          Create
+        </h1>
+        <p className='mt-2 text-[#666e75] text-[16px] max-w[500px]'>
+          Create imaginative and visually stunning images through DALL-E AI and share them with the community
+        </p>
+      </div>
+      <form className='mt-16 max-w-3xl' onSubmit={handleSubmit}>
+        <div className='flex flex-col gap-5'>
+          <FormField
+            LabelName="Your name"
+            type="text"
+            name="name"
+            placeholder="John Doe"
+            value={form.name}
+            handleChange={handleChange}
+          />
+          <FormField
+            LabelName="Prompt"
+            type="text"
+            name="prompt"
+            placeholder="An astronaut encountering an alien life form on a distant planet, photography"
+            value={form.prompt}
+            handleChange={handleChange}
+            isSurpriseMe
+            handleSurpriseMe={handleSurpriseMe}
+          />
+        </div>
+      </form>
+    </section>
   )
 }
 
